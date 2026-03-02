443b05345c37cad85b3297ccb3cfda95adf5f1b4
Peter Rice <peterrice@fastmail.com>
2026-03-02 00:48:00 -0500

    flake

---
+++ flake.nix
@@ -0,0 +1,66 @@
+{
+  description = "Zig implementation of Lox, from the book Crafting Interpreters";
+
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      forAllSystems =
+        mkOutputs:
+        nixpkgs.lib.genAttrs [
+          "aarch64-linux"
+          "aarch64-darwin"
+          "x86_64-darwin"
+          "x86_64-linux"
+        ] (system: mkOutputs nixpkgs.legacyPackages.${system});
+    in
+    {
+      packages = forAllSystems (pkgs: rec {
+        lox = pkgs.stdenv.mkDerivation {
+          pname = "lox";
+          version = "0.0.1";
+          src = ./.;
+          #     dontConfigure = true;
+          #     buildInputs = [ pkgs.janet ];
+          #     nativeBuildInputs = [ pkgs.makeWrapper ];
+          #     installPhase = ''
+          #       mkdir $out
+          #       JANET_PATH=$out janet --install .
+          #       mkdir -p $out/bin
+          #       mv bin/lox $out/bin/lox
+          #       wrapProgram $out/bin/lox --set JANET_PATH $out
+          #     '';
+          #     doInstallCheck = true;
+          #     installCheckPhase = ''
+          #       $out/bin/lox << EOF
+          #       fun add(x, y) {
+          #         return x + y;
+          #       }
+          #       print add(10, 5);
+          #       EOF | grep 15
+          #     '';
+          meta.mainProgram = "lox";
+        };
+        default = lox;
+      });
+
+      apps = forAllSystems (pkgs: {
+        lox = {
+          type = "app";
+          program = pkgs.lib.getExe self.packages.${pkgs.system}.lox;
+        };
+        default = self.apps.${pkgs.system}.lox;
+      });
+
+      devShells = forAllSystems (pkgs: {
+        default = pkgs.mkShell {
+          packages = with self.packages.${pkgs.system}; [
+            pkgs.zig
+            pkgs.zls
+            # lox
+          ];
+        };
+      });
+    };
+}
