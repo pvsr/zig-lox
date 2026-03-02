e5186f804a118b943a66ce057a973a6d37720f36
Peter Rice <peterrice@fastmail.com>
2026-03-02 02:42:03 -0500

    chapter 14

---
+++ src/chunk.zig
@@ -0,0 +1,27 @@
+const std = @import("std");
+const Value = @import("value.zig").Value;
+
+pub const OpCode = enum(u8) {
+    op_constant,
+    op_return,
+    _,
+};
+
+pub const Chunk = struct {
+    code: std.ArrayList(u8),
+    constants: std.ArrayList(Value),
+    lines: std.ArrayList(u32),
+
+    pub fn init() Chunk {
+        return Chunk{
+            .code = std.ArrayList(u8).empty,
+            .constants = std.ArrayList(Value).empty,
+            .lines = std.ArrayList(u32).empty,
+        };
+    }
+
+    pub fn write(self: *Chunk, gpa: std.mem.Allocator, byte: u8, line: u32) !void {
+        try self.code.append(gpa, byte);
+        try self.lines.append(gpa, line);
+    }
+};
---
+++ src/main.zig
@@ -0,0 +1,26 @@
+const std = @import("std");
+const ArrayList = std.ArrayList;
+const chunk = @import("chunk.zig");
+const debug = @import("debug.zig");
+const value = @import("value.zig");
+
+pub fn main() !void {
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+    const allocator = arena.allocator();
+    var c = chunk.Chunk.init();
+    try c.constants.append(allocator, 2.1);
+    try c.constants.append(allocator, 1);
+    try c.constants.append(allocator, 0);
+    try c.write(allocator, @intFromEnum(chunk.OpCode.op_return), 0);
+    try c.write(allocator, @intFromEnum(chunk.OpCode.op_constant), 1);
+    try c.write(allocator, 0, 1);
+    try c.write(allocator, @intFromEnum(chunk.OpCode.op_constant), 2);
+    try c.write(allocator, 1, 2);
+    try c.write(allocator, @intFromEnum(chunk.OpCode.op_constant), 3);
+    try c.write(allocator, 2, 3);
+    try c.write(allocator, @intFromEnum(chunk.OpCode.op_constant), 3);
+    try c.write(allocator, 0, 3);
+    try c.write(allocator, @intFromEnum(chunk.OpCode.op_return), 3);
+    debug.disassembleChunk(c, "test chunk");
+}
