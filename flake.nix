36852af77f3f685f3ae06a2a49de0fc32b5c6b49
Peter Rice <peterrice@fastmail.com>
2026-04-14 20:00:58 -0400

    temporarily build with nixpkgs fork for zig 0.16

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "Zig implementation of Lox, from the book Crafting Interpreters";
 
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  inputs.nixpkgs.url = "github:ddogfoodd/nixpkgs/update-zig";
 
   outputs =
     { self, nixpkgs }:
@@ -17,20 +17,22 @@
     in
     {
       packages = forAllSystems (
-        system: pkgs: {
+        system: pkgs:
+        let
+          zig = pkgs.zig_0_16;
+        in
+        {
           zlox = pkgs.stdenv.mkDerivation (finalAttrs: {
             pname = "zlox";
             version = "0.0.1";
             src = nixpkgs.lib.cleanSource ./.;
-            nativeBuildInputs = [ pkgs.zig ];
+            nativeBuildInputs = [ zig ];
             meta.mainProgram = "zlox";
             doCheck = true;
-            zigDeps = (
-              pkgs.zig.fetchDeps {
-                inherit (finalAttrs) src pname version;
-                hash = "sha256-uA/s+JPpKlvPNUkFkwZpR/SAU6CakOKruOX04nCtNP0=";
-              }
-            );
+            zigDeps = zig.fetchDeps {
+              inherit (finalAttrs) src pname version;
+              hash = "sha256-cX1Ev7h7IpXN9q7/58Rqs2q4Mz8M2BtLYLSXjjpmcOc=";
+            };
             postConfigure = ''
               ln -s ${finalAttrs.zigDeps} "$ZIG_GLOBAL_CACHE_DIR/p"
             '';
@@ -64,7 +66,7 @@
         _: pkgs: {
           default = pkgs.mkShell {
             packages = [
-              pkgs.zig
+              pkgs.zig_0_16
               pkgs.zls
             ];
           };
